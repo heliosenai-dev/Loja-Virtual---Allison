0d76cdb12f84e9e8d45fc543139002a3295c2669
Hélio Marcos <h.paixao@aluno.senai.br>
2026-07-13 22:52:13 -0300

    Pegando section do DOM e criado a função listarProdutos

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -1,1 +1,11 @@
 import { produtos } from "./produtos.js";
+
+//PEGANDO ELEMENTO DO DOM
+const section_cards = document.querySelector(#cards)
+
+//FUNÇÃO PARA CARREGAR OS PRODUTOS
+const listarProdutos = ()=>{
+    produtos.forEach((elem, i)=>{
+
+    })
+}
