ced7b16fcdd91c80f76adadabcd1ea53aafb4f10
Hélio Marcos <h.paixao@aluno.senai.br>
2026-07-15 12:37:22 -0300

    Criado as funções de filtragem e montar link

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -37,6 +37,61 @@ const listarProdutos = ()=>{
         section_cards.appendChild(divCard)
 
     })
+
 }
 
 listarProdutos()
+
+//FILTRANDO AS SEÇÕES COM A COLEÇÃO map
+const listarSecoes = () => {
+    //CRIANDO A COLEÇÃO MAP
+    const secoesFiltrada = new Map()
+
+    //PECORRENDO O ARRAY PRODUTOS E FILTRANDO AS SEÇÕES
+    produtos.forEach((elem, i) => {
+        //CRIANDO A CHAVE E O VALOR DA COLEÇÃO MAP A PARTIR DO ID DA SEÇÃO DA LISTA DE PRODUTOS
+        secoesFiltrada.set(elem.id_secao, elem)
+    })
+
+    //CONVERTENDO O MAP EM ARRAY
+    const secoesMenu = Array.from(secoesFiltrada.values())
+
+    //RETORNADO O ARRAY CONVERTIDO
+    return secoesMenu
+
+}
+
+//MONTANDO OS LINKS SEÇÕES
+const montarSecoes = () => {
+    //PEGANDO O ELEMENTO DO DOM
+    const ulMenu = document.querySelector('#menu-secoes')
+    //LIMPANDO O ELEMENTO ulMenu
+    ulMenu.innerHTML = ''
+
+    //PERCORRENDO O ARRAY DAS SEÇÕES FILTRADA
+    listarSecoes().forEach((elem, i) => {
+        //CRIANDO O ELEMENTO li
+        const liSecao = document.createElement('li')
+
+        //CRIANDO O ELEMENTO a
+        const aSecao = document.createElement('a')
+        aSecao.setAttribute('href', '#')
+        aSecao.setAttribute('class', 'lnk-secao')
+        aSecao.innerHTML = elem.nome_secao
+
+        //CAPTURANDO O CLICK DOS LINKS
+        aSecao.addEventListener('click', () => {
+            //PARA TESTE 
+            console.log(elem.id_secao)
+        })
+
+        //ADICIONANDO O ELEMENTO FILHO a NO ELEMENTO li
+        liSecao.appendChild(aSecao)
+
+        //ADICIONANDO O ELEMENTO FILHO li NO ELEMENTO DO DOM ul
+        ulMenu.appendChild(liSecao)
+    })
+
+}
+
+montarSecoes()
